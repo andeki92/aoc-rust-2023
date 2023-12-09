c7084dc0310edb9db761b47353d43182e7d56760
Anders Klever Kirkeby <anders.klever.kirkeby@statnett.no>
2023-12-09 07:48:08 +0100

    feat: add num lib to util and split sub-crates

---
+++ utils/src/string.rs
@@ -0,0 +1,38 @@
+use std::{fs::File, io::Read};
+
+pub fn read(file_name: &str) -> Vec<String> {
+    let mut f = File::open(file_name).expect(&format!("file not found: {}", file_name));
+
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)
+        .expect(&format!("cannot read file {}", file_name));
+
+    contents
+        .trim_end()
+        .split("\n")
+        .map(|s| s.to_owned())
+        .collect()
+}
+
+pub fn read_string(file_name: &str) -> String {
+    let mut f = File::open(file_name).expect(&format!("file not found: {}", file_name));
+
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)
+        .expect(&format!("cannot read file {}", file_name));
+
+    contents.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reading_a_file_returns_its_content() {
+        let content = read("src/lib.rs");
+        assert!(content.contains(&"use std::{fs::File, io::Read};".to_owned()))
+    }
+}
